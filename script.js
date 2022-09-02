b67cb701ff93e4254c2168678cea9b4b7f6bd090
Andrei <andrei.ivanuta@gmail.com>
2022-09-02 07:12:10 +0300

    Pancake Restaurant First Design

---
+++ script.js
@@ -0,0 +1,8 @@
+const but1 = document.getElementById('but1');
+const hidshow = document.getElementById('mor');
+
+
+but1.onclick = function despreNoi() {
+    hidshow.style.visibility = "visible";
+}
+
