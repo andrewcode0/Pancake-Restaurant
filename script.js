7efee5cf8e84eda458fcf5fa0b75c3f4114eb779
andrei <andrei.ivanuta@gmail.com>
2022-09-07 17:52:09 +0300

    Added buttons, footer, and basic info

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const but1 = document.getElementById('but1');
-const hidshow = document.getElementById('mor');
+const hidshow1 = document.getElementById('c1');
+const hidshow2 = document.getElementById('c2');
+const hidshow3 = document.getElementById('c3');
 
 
 but1.onclick = function despreNoi() {
-    hidshow.style.visibility = "visible";
+    if(hidshow1.style.visibility == "hidden"){
+    hidshow1.style.visibility = "visible";
+    } else hidshow1.style.visibility = "hidden";
 }
 
+but2.onclick = function meniu() {
+    if(hidshow2.style.visibility == "hidden"){
+        hidshow2.style.visibility = "visible";
+        } else hidshow2.style.visibility = "hidden";
+}
+but3.onclick = function contact() {
+    if(hidshow3.style.visibility == "hidden"){
+        hidshow3.style.visibility = "visible";
+        } else hidshow3.style.visibility = "hidden";
+}
